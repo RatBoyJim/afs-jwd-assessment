e267cc80b2a541c0ffb34d8e889688317909b9eb
RatBoyJim <>
2021-03-31 17:41:00 +0100

    routes created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
+import { ClientsComponent } from './home/clients/clients.component';
+import { EmployeesComponent } from './home/employees/employees.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'home/clients', component: ClientsComponent },
+  { path: 'home/employees', component: EmployeesComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

b32601dd73119395fe22dfa39df61bfde5fc7586
RatBoyJim <>
2021-03-31 17:21:08 +0100

    home module added to app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+
+import { HomeModule } from './home/home.module'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { ClientsComponent } from './clients/clients.component';
+import { EmployeesComponent } from './employees/employees.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent, ClientsComponent, EmployeesComponent],
+  exports: [HomeComponent],
   imports: [
     CommonModule
   ]

097c7b3c0a1cec17d8c91c765a82bfd4fecbd6e3
RatBoyJim <>
2021-03-31 19:28:43 +0100

    added guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
 import { ClientsComponent } from './home/clients/clients.component';
 import { EmployeesComponent } from './home/employees/employees.component';
+import { AuthGuard } from './auth/auth.guard'
 
 const routes: Routes = [
   { 
     path: 'home', 
     component: HomeComponent,
+    canActivate: [AuthGuard],
     children: [
-      { path: 'home/clients', component: ClientsComponent },
-      { path: 'home/employees', component: EmployeesComponent }
+      { path: 'home/clients', component: ClientsComponent},
+      { path: 'home/employees', component: EmployeesComponent}
     ]
   },
   { path: '', redirectTo: 'home', pathMatch: 'full' },

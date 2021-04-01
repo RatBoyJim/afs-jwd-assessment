46878c97fedcc9ee9726b6f9c7505aecc396565a
RatBoyJim <>
2021-04-01 10:22:46 +0100

    added ant-design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 
-import { HomeModule } from './home/home.module'
+import { HomeModule } from './home/home.module';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -15,9 +24,12 @@ import { HomeModule } from './home/home.module'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeModule
+    HomeModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

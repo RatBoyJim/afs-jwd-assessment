427638a5fbeabe527027453f75b09d24ff03bc0f
RatBoyJim <>
2021-04-02 14:05:01 +0100

    attempt to ammend styling

---
+++ extra-webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test   : /\.less$/,
+        loader: 'less-loader',
+        options: {
+          modifyVars: { // modify theme variable
+            'primary-color': '#F5a624',
+            'link-color': '#1DA57A',
+            'border-radius-base': '2px'
+          },
+          javascriptEnabled: true
+        }
+      }
+    ]
+  }
+};

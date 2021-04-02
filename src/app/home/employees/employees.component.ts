016cabe8783d21b2eed4f8fc33a2eb61bf37dd85
RatBoyJim <>
2021-04-02 11:36:08 +0100

    query working

--- src/app/home/employees/employees.component.ts
+++ src/app/home/employees/employees.component.ts
@@ -5,19 +5,25 @@ import * as queries from '../../../graphql/queries';
 import * as mutations from '../../../graphql/mutations';
 import * as subscriptions from '../../../graphql/subscriptions';
 
-
-
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
   styleUrls: ['./employees.component.scss']
 })
 export class EmployeesComponent implements OnInit {
+  
+  allTodos:any;
 
   constructor() { }
 
-  ngOnInit(): void {
-    
+  async ngOnInit() {
+    this.allTodos = await (API.graphql({ query: queries.listTodos }));
+    // const allTodos = await API.graphql(graphqlOperation(getAllTodos), authMode: 'AWS_IAM');
+    console.log(this.allTodos);
+  }
+
+  async listTodos(){
+    this.allTodos = await (API.graphql({ query: queries.listTodos }));
   }
 
 }
